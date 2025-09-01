eaee64bd5a0794265e9522adfc0a64c0283271d5
Aurpan <aurpan.dash@dsinnovators.com>
2025-09-01 21:27:19 +0600

    Solution of Assignment 1

---
+++ Assignment1/Car.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment1
+{
+    internal class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+
+        public void Drive(int km)
+        {
+            Mileage += km; // Mileage = Mileage + km
+            Console.WriteLine($"Car {Brand} {Model} has driven {Mileage} km."); // string interpolation
+            //Console.WriteLine("Car " + Brand + " " + Model + " " + "has driven " + Mileage + " " + "km.");
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Car Info: Brand - {Brand}, Model - {Model}, Year - {Year}, Mileage - {Mileage} ");
+        }
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car toyota = new Car();
+            Car mazda = new Car();
+
+            toyota.Brand = "Toyota";
+            toyota.Model = "Corolla";
+            toyota.Year = 2020;
+            toyota.Mileage = 15000;
+
+            mazda.Brand = "Mazda";
+            mazda.Model = "RX-8";
+            mazda.Year = 2018;
+            mazda.Mileage = 30000;
+
+            toyota.Drive(1000);
+            mazda.Drive(2000);
+        }
+    }
+}
